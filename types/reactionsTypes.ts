0700502bef5e0abc0496d78f3eb24f9da4afb6e6
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-10-11 11:46:23 +0200

    components/profileFeed.tsx types fixed working on development

--- types/reactionsTypes.ts
+++ types/reactionsTypes.ts
@@ -1,48 +1,8 @@
 export type ReactionsType = {
-    reactions: {
-        Row: {
             created_at: string;
             emoji: string;
             post_id: string;
             react_giver_user_id: string;
             react_recipient_user_id: string;
             reaction_id: string;
-        };
-        Insert: {
-            created_at?: string;
-            emoji: string;
-            post_id: string;
-            react_giver_user_id: string;
-            react_recipient_user_id: string;
-            reaction_id?: string;
-        };
-        Update: {
-            created_at?: string;
-            emoji?: string;
-            post_id?: string;
-            react_giver_user_id?: string;
-            react_recipient_user_id?: string;
-            reaction_id?: string;
-        };
-        Relationships: [
-            {
-                foreignKeyName: "reactions_post_id_fkey";
-                columns: ["post_id"];
-                referencedRelation: "posts";
-                referencedColumns: ["post_id"];
-            },
-            {
-                foreignKeyName: "reactions_react_giver_user_id_fkey";
-                columns: ["react_giver_user_id"];
-                referencedRelation: "users";
-                referencedColumns: ["id"];
-            },
-            {
-                foreignKeyName: "reactions_react_recipient_user_id_fkey";
-                columns: ["react_recipient_user_id"];
-                referencedRelation: "users";
-                referencedColumns: ["id"];
-            },
-        ];
-    };
 };
