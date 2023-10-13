bc04f351b3a2cfb317dac89c1d2a5108711548d5
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-10-13 15:49:33 +0200

    profile page compose post avatar fixed to current logged user

--- app/u/[author_id]/page.tsx
+++ app/u/[author_id]/page.tsx
@@ -3,6 +3,7 @@ import AppLayout from "@/app/layouts/page";
 import NavBar from "@/components/NavBar";
 import ComposePost from "@/components/compose-post";
 import ProfileFeed from "@/components/profileFeed";
+import { getUserSession } from "@/utils/userSessionUtils";
 
 type AuthorPageType = {
     params: {
@@ -10,13 +11,17 @@ type AuthorPageType = {
     };
 };
 
-const AuthorPage = ({params}: AuthorPageType) => {
-  console.log("params: ", params.author_id);  
+const AuthorPage = async ({params}: AuthorPageType) => {
+  const sessionData = await getUserSession(); 
+  if (sessionData === null) return null;
+
+  const { userProfilePic } = sessionData;
+
   return (
       <div className="w-full flex flex-col items-center">
         <NavBar />
         <AppLayout>
-            <ComposePost profile_pic={"/icons/user.png"} /> 
+            <ComposePost profile_pic={userProfilePic} /> 
             <ProfileFeed type={"profile"} author_id={params.author_id}/>
         </AppLayout>
       </div>
